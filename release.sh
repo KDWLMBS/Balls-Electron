d07e349a82f80da1b15a77b99622f4089ec0eb98
David Oberhofer <oberhofer.david@gmail.com>
2017-09-16 10:31:39 +0200

    build script fixes

--- release.sh
+++ release.sh
@@ -5,8 +5,6 @@ npm i --loglevel=warn
 (cd web && npm i --loglevel=warn && ./node_modules/.bin/au build --env prod)
 rm -rf release
 mkdir -p release
-mkdir -p release/web
-cp -r resources dist module.js package.json release
-cp -r web/index.html web/scripts release/web
+cp -r resources dist web package.json release
 (cd release && npm install --production)
 (cd release && node_modules/.bin/electron-packager . --platform=win32 --arch=x64)
